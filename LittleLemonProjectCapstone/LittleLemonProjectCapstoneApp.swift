54aa372bd23a33f7f5f7b3368971280372694bd9
AdnannM <adnannmuratovic@icloud.com>
2024-10-16 20:15:18 +0200

    Initial Commit

---
+++ LittleLemonProjectCapstone/LittleLemonProjectCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonProjectCapstoneApp.swift
+//  LittleLemonProjectCapstone
+//
+//  Created by Adnann Muratovic on 16.10.24.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonProjectCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
