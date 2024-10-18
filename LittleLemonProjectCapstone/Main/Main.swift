92ed086f4feaca90ea995f8228d4221c35816eca
AdnannM <adnannmuratovic@icloud.com>
2024-10-18 13:46:08 +0200

    add design for main view

--- LittleLemonProjectCapstone/Main/Main.swift
+++ LittleLemonProjectCapstone/Main/Main.swift
@@ -13,22 +13,51 @@ struct Main: View {
     var body: some View {
         VStack(spacing: 0) {
             // Logo and header section
-            VStack {
-                LittleLemonLogo()
-                Text("Little Lemon")
-                Text("Chicago IL")
-                Text("Best restaurant in town")
+            MainHeader()
+            MainHero()
+            
+            VStack(alignment: .leading) {
+                Text("Order for Delivery")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding([.top, .leading], 20)
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing: 20) {
+                        ForEach(1...5, id: \.self) { _ in
+                            Text("Buttons")
+                                .styledText()
+                        }
+                    }
+                    .padding()
+                }
+                .frame(maxWidth: .infinity, maxHeight: 50)
             }
-            .padding()
+            
+            Divider().padding([.leading, .trailing, .top])
             
             // Navigation section with search and list
             NavigationView {
                 List {
-                    Text("Food Item 1")
-                    Text("Food Item 2")
-                    Text("Food Item 3")
-                    Text("Food Item 4")
-                    Text("Food Item 5")
+                    HStack(spacing: 25) {
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("Greek Salad")
+                                .fontWeight(.bold)
+                            Text("The famous greek salad of crispy lettuce, peppers, olives, our Chicago.")
+                                .foregroundStyle(.gray)
+                                .fontWeight(.bold)
+                                .font(.caption)
+                                .multilineTextAlignment(.leading)
+                            Text("$ 10")
+                                .fontWeight(.bold)
+                        }
+                        
+                        Image("testImage")
+                            .resizable()
+                            .frame(width: 130, height: 100)
+                            .background(.red)
+                            .cornerRadius(20)
+                    }
                 }
                 .listStyle(.plain)
                 .searchable(text: $text)
@@ -41,3 +70,6 @@ struct Main: View {
 #Preview {
     Main()
 }
+
+
+
---
+++ LittleLemonProjectCapstone/Main/MainHero.swift
@@ -0,0 +1,60 @@
+//
+//  MainHero.swift
+//  LittleLemonProjectCapstone
+//
+//  Created by Adnann Muratovic on 18.10.24.
+//
+
+import Foundation
+import SwiftUI
+
+struct MainHero: View {
+    var body: some View {
+        ZStack {
+            Color.primary1
+            HStack() {
+                VStack(alignment: .leading, spacing: 15) {
+                    // Restaurant location
+                    VStack(alignment: .leading) {
+                        Text("Little Lemon")
+                            .font(.system(size: 30, weight: .bold))
+                            .foregroundStyle(Color.primary2)
+                        
+                        Text("Chicago, IL")
+                            .font(.title3)
+                            .foregroundStyle(.white)
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.leading)
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        // Short description
+                        Text("We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.")
+                            .font(.subheadline)
+                            .foregroundStyle(Color.secondary3)
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.top, 10)
+                
+                VStack {
+                    Image("heroImage")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 130, height: 150)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.secondary2.opacity(0.1), lineWidth: 2)
+                        )
+                        .onSubmit {
+                            print("Clicked")
+                        }
+                }
+            }
+            .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: 220)
+    }
+}
